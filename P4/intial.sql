3c6c9c46a682875ca49cf96a03dd6c7f4e2cb09d
amireddym <amireddy.m@northeastern.edu>
2023-03-27 15:42:50 -0400

    Added all the tables DDL commands

--- P4/intial.sql
+++ P4/intial.sql
@@ -44,3 +44,195 @@ ALTER TABLE [dbo].[staff] CHECK CONSTRAINT [foreign_staff]
 GO
 
 
+CREATE TABLE [dbo].[event](
+    [event_id] [int] NOT NULL,
+    [event_name] [varchar](25) NOT NULL,
+    [event_date] [date] NOT NULL,
+    [event_location] [varchar](50) NULL,
+    [event_length] [int] NULL,
+    [hotel_id] [int] NOT NULL
+ CONSTRAINT [prim_event] PRIMARY KEY CLUSTERED
+(
+    [event_id] ASC
+) 
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[event]  WITH CHECK ADD  CONSTRAINT [foreign_event] FOREIGN KEY([hotel_id])
+REFERENCES [dbo].[hotel] ([hotel_id])
+GO
+ALTER TABLE [dbo].[event] CHECK CONSTRAINT [foreign_event]
+GO
+
+CREATE TABLE [dbo].[customer](
+    [customer_id] [int] NOT NULL,
+    [customer_ssn] [varchar](10) NULL,
+    [customer_name] [varchar](25) NOT NULL,
+    [customer_contact] [varchar](10) NULL,
+    [customer_address] [varchar](50) NULL,
+ CONSTRAINT [prim_customer] PRIMARY KEY CLUSTERED
+(
+    [customer_id] ASC
+) 
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[room_type](
+    [room_type_id] [int] NOT NULL,
+    [price] [int] NOT NULL,
+    [description] [varchar](50) NULL
+    CONSTRAINT [prim_room_type] PRIMARY KEY CLUSTERED
+(
+    [room_type_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[amenity](
+    [amenity_id] [int] NOT NULL,
+    [amenity] [varchar](50) NULL
+    CONSTRAINT [prim_amenity] PRIMARY KEY CLUSTERED
+(
+    [amenity_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[room_type_amenity](
+    [room_type_amenity_id] [int] NOT NULL,
+    [amenity_id] [int] NOT NULL,
+    [room_type_id] [int] NOT NULL
+    CONSTRAINT [prim_room_type_amenity] PRIMARY KEY CLUSTERED
+(
+    [room_type_amenity_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[room_type_amenity]  WITH CHECK ADD  CONSTRAINT [foreign_amenity] FOREIGN KEY([amenity_id])
+REFERENCES [dbo].[amenity] ([amenity_id])
+GO
+ALTER TABLE [dbo].[room_type_amenity] CHECK CONSTRAINT [foreign_amenity]
+GO
+
+ALTER TABLE [dbo].[room_type_amenity]  WITH CHECK ADD  CONSTRAINT [foreign_room_type] FOREIGN KEY([room_type_id])
+REFERENCES [dbo].[room_type] ([room_type_id])
+GO
+ALTER TABLE [dbo].[room_type_amenity] CHECK CONSTRAINT [foreign_room_type]
+GO
+
+
+CREATE TABLE [dbo].[room](
+    [room_id] [int] NOT NULL,
+    [room_number] [int] NOT NULL,
+    [availability] [bit] NOT NULL,
+    [room_type_id] [int] NOT NULL,
+    [hotel_id] [int] NOT NULL
+    CONSTRAINT [prim_room] PRIMARY KEY CLUSTERED
+(
+    [room_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[room]  WITH CHECK ADD  CONSTRAINT [foreign_hotel] FOREIGN KEY([hotel_id])
+REFERENCES [dbo].[hotel] ([hotel_id])
+GO
+ALTER TABLE [dbo].[room] CHECK CONSTRAINT [foreign_hotel]
+GO
+
+ALTER TABLE [dbo].[room]  WITH CHECK ADD  CONSTRAINT [foreign_room_room_type] FOREIGN KEY([room_type_id])
+REFERENCES [dbo].[room_type] ([room_type_id])
+GO
+ALTER TABLE [dbo].[room] CHECK CONSTRAINT [foreign_room_room_type]
+GO
+
+
+CREATE TABLE [dbo].[reservation](
+    [reservation_id] [int] NOT NULL,
+    [check_in] [date] NOT NULL,
+    [check_out] [date] NOT NULL,
+    [duration] [int] NOT NULL,
+    [reservation_date] [date] NOT NULL,
+    [customer_id] [int] NOT NULL,
+    [hotel_id] [int] NOT NULL
+    CONSTRAINT [prim_reservation] PRIMARY KEY CLUSTERED
+(
+    [reservation_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[reservation]  WITH CHECK ADD  CONSTRAINT [foreign_reservation_hotel] FOREIGN KEY([hotel_id])
+REFERENCES [dbo].[hotel] ([hotel_id])
+GO
+ALTER TABLE [dbo].[reservation] CHECK CONSTRAINT [foreign_reservation_hotel]
+GO
+
+ALTER TABLE [dbo].[reservation]  WITH CHECK ADD  CONSTRAINT [foreign_customer] FOREIGN KEY([customer_id])
+REFERENCES [dbo].[customer] ([customer_id])
+GO
+ALTER TABLE [dbo].[reservation] CHECK CONSTRAINT [foreign_customer]
+GO
+
+
+CREATE TABLE [dbo].[reservation_room](
+    [reservation_room_id] [int] NOT NULL,
+    [reservation_id] [int] NOT NULL,
+    [room_id] [int] NOT NULL,
+    [status] [bit] NOT NULL
+    CONSTRAINT [prim_reservation_room] PRIMARY KEY CLUSTERED
+(
+    [reservation_room_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[reservation_room]  WITH CHECK ADD  CONSTRAINT [foreign_reservation] FOREIGN KEY([reservation_id])
+REFERENCES [dbo].[reservation] ([reservation_id])
+GO
+ALTER TABLE [dbo].[reservation_room] CHECK CONSTRAINT [foreign_reservation]
+GO
+
+ALTER TABLE [dbo].[reservation_room]  WITH CHECK ADD  CONSTRAINT [foreign_room] FOREIGN KEY([room_id])
+REFERENCES [dbo].[room] ([room_id])
+GO
+ALTER TABLE [dbo].[reservation_room] CHECK CONSTRAINT [foreign_room]
+GO
+
+CREATE TABLE [dbo].[payment](
+    [payment_id] [int] NOT NULL,
+    [payment_date] [date] NOT NULL,
+    [payment_method] [varchar](10) NOT NULL,
+    [reservation_id] [int] NOT NULL
+    CONSTRAINT [prim_payment] PRIMARY KEY CLUSTERED
+(
+    [payment_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[payment]  WITH CHECK ADD  CONSTRAINT [foreign_payment_reservation] FOREIGN KEY([reservation_id])
+REFERENCES [dbo].[reservation] ([reservation_id])
+GO
+ALTER TABLE [dbo].[payment] CHECK CONSTRAINT [foreign_payment_reservation]
+GO
+
+CREATE TABLE [dbo].[transaction](
+    [transaction_id] [int] NOT NULL,
+    [transaction_name] [varchar](10) NOT NULL,
+    [transaction_date] [date] NOT NULL,
+    [transaction_amount] [int] NOT NULL,
+    [payment_id] [int] NOT NULL
+    CONSTRAINT [prim_transaction] PRIMARY KEY CLUSTERED
+(
+    [transaction_id] ASC
+)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[transaction]  WITH CHECK ADD  CONSTRAINT [foreign_payment] FOREIGN KEY([payment_id])
+REFERENCES [dbo].[payment] ([payment_id])
+GO
+ALTER TABLE [dbo].[transaction] CHECK CONSTRAINT [foreign_payment]
+GO
